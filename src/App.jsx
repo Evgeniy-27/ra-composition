87e59a97a6e162a3fad2b4eeb424c046e75732d4
Evgeniy <Li.Evgen@yandex.ru>
2021-06-24 23:32:39 +0700

    Готова задача 1

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+
+import ColorForm from "./task1/Cards.jsx";
+// import InputForm from "./task2/InputForm";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="task-1">
+        <ColorForm />
+      </div>
+      <div className="task-2">
+     
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default App;
